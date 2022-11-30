23af9cc054d40b58f9881e72a532f1d998461a5a
maxym-huklivsky <maksimgukl@gmail.com>
2022-11-30 17:29:49 +0200

    main work

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import axios from 'axios';
-import { Notify } from 'notiflix';
+import Notiflix from 'notiflix';
 import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const API_KEY = '31703269-63db8468fb2ed221e4e93ddb7';
-const BASE_URL = 'https://pixabay.com/api';
+const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 40;
+let page = 1;
+let inputValue = null;
+let totalCards = null;
 
 const form = document.querySelector('.search-form');
+const loadButton = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
 
-form.addEventListener('submit', onSearchPosts);
+// По сабміту
+form.addEventListener('submit', onSearchCards);
 
-function onSearchPosts(e) {
+function onSearchCards(e) {
   e.preventDefault();
 
-  const inputValue = e.currentTarget.elements.searchQuery.value;
+  gallery.innerHTML = '';
+
+  page = 1;
+
+  inputValue = e.currentTarget.elements.searchQuery.value;
 
   const params = {
     key: API_KEY,
@@ -20,8 +32,147 @@ function onSearchPosts(e) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 40,
+    per_page: PER_PAGE,
+    page,
   };
 
-  axios.get(BASE_URL, { params }).then(data => console.log(data));
+  loadButton.removeAttribute('disabled');
+
+  axios.get(BASE_URL, { params }).then(cards => {
+    console.log(cards.data.totalHits);
+
+    totalCards = cards.data.totalHits;
+
+    if (cards.data.hits.length === 0) {
+      return Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+
+    Notiflix.Notify.success(`"Hooray! We found ${totalCards} images."`);
+
+    const markup = cards.data.hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<div class="photo-card">
+          <a class="gallery__link" href="${largeImageURL}">
+        <img
+          src="${webformatURL}"
+          alt="${tags}"
+          class="gallery-pic"
+          loading="lazy"
+        />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+          </p>
+        </div>
+        </a>
+      </div>`;
+        }
+      )
+      .join(',');
+    gallery.insertAdjacentHTML('beforeend', markup);
+
+    gallerySLB.refresh();
+  });
 }
+
+// Загрузити ще
+loadButton.addEventListener('click', onLoadMoreCards);
+
+function onLoadMoreCards() {
+  if (totalCards <= page * PER_PAGE) {
+    return Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  page += 1;
+
+  const params = {
+    key: API_KEY,
+    q: inputValue,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: PER_PAGE,
+    page,
+  };
+
+  axios.get(BASE_URL, { params }).then(cards => {
+    console.log(cards.data.totalHits);
+
+    const markup = cards.data.hits
+      .map(
+        ({
+          webformatURL,
+          largeImageURL,
+          tags,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `<div class="photo-card">
+          <a class="gallery__link" href="${largeImageURL}">
+        <img
+          src="${webformatURL}"
+          alt="${tags}"
+          class="gallery-pic"
+          loading="lazy"
+        />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes: ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views: ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments: ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+          </p>
+        </div>
+        </a>
+      </div>`;
+        }
+      )
+      .join(',');
+    gallery.insertAdjacentHTML('beforeend', markup);
+
+    gallerySLB.refresh();
+  });
+}
+
+const gallerySLB = new simpleLightbox('.gallery a');
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
+
+window.scrollBy(0, window.innerHeight);
